2045f97df12de15f6d2c1b79b82c644897b2496b
parth2903 <parthkansal2002@gmail.com>
2024-12-16 15:41:56 +0530

    Auth start

--- frontend/src/app/myComp/header/header.component.ts
+++ frontend/src/app/myComp/header/header.component.ts
@@ -2,21 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../service/category.service';
 import { Category } from '../../models/category.model';
 import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit{
   categoryList : Category[] = []
-  constructor(private categoryService: CategoryService){}
+  constructor(private categoryService: CategoryService, private router: Router){}
 
   ngOnInit(): void {
       this.categoryService.getCategories().subscribe(result => {
         this.categoryList = result;
       })
+  }
+
+  onSearch(e: any){
+    if(e.target.value){
+      this.router.navigateByUrl("/products?search="+e.target.value)
+    }
+    
+  }
+
+  onCategoryClick(id: String){
+    this.router.navigateByUrl("/products?categoryId="+id)
   }
 }
